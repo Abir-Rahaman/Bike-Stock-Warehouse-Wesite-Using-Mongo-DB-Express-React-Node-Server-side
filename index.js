42ba816e8c1ca73a47d585a55075fd1ba6bfd4a2
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-05-05 19:11:50 +0600

    find one oparation done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express()
 require ('dotenv').config();
 const port = process.env.PORT||5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -19,20 +19,30 @@ async function run() {
     await client.connect();
     const bikeCollection = client.db("bike_data").collection("bikewarehouse");
     // console.log("db-connected");
+    app.get('/bikes/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await bikeCollection.findOne(query);
+      res.send(result)
+  })
 
 
     // get api to read all bikeData
+    // http://localhost:5000/bikes
     app.get("/bikes" , async(req,res)=>{
-      console.log(req);
+      // console.log(req);
       const query = {};
       const cursor = bikeCollection.find(query);
-      const result = await cursor.toArray();
+      const result = await cursor.limit(6).toArray();
       res.send(result);
-
-
-
-      res.send("send")
     })
+
+
+
+
+   // get api to  single bikeData
+   
+
 
   }
 
@@ -49,7 +59,7 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Eid Mubarak')
 })
 
 app.listen(port, () => {
